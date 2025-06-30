69e565ed236c9ead745134b20eb1de2756551345
tommberg <danieltomberg.work@gmail.com>
2025-06-30 15:25:25 +0300

    corrected config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import vite from "vite-plugin-next-refresh";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), vite()],
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       "/api": "http://localhost:3099",
